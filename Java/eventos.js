d0aeded47e46a842b23aeeba73f5d271fc881da0
guitardafo <guitardafo@gmail.com>
2019-11-18 00:15:30 -0500

    first commit

---
+++ Java/eventos.js
@@ -0,0 +1,47 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+function abrir(){
+		document.getElementById("Modala1").style.display="block";
+	}
+	function cerrar(){
+		document.getElementById("Modala1").style.display="none";
+	}
+	function aparecer(){
+		document.getElementById("Modalb2").style.display="block";
+	}
+	function desaparecer(){
+		document.getElementById("Modalb2").style.display="none";
+	}
+	function aceptar(){
+		document.getElementById("Modalc3").style.display="block";
+	}	
+	function rechazar(){
+		document.getElementById("Modalc3").style.display="none";
+	}
+	function open(){
+		document.getElementById("Modald4").style.display="block";
+	}
+	function close(){
+		document.getElementById("Modald4").style.display="none";
+	}
+	function visible(){
+		document.getElementById("Modale5").style.display="block";
+	}
+	function invisible(){
+		document.getElementById("Modale5").style.display="none";
+	}
+	function ok(){
+		document.getElementById("Modalf6").style.display="block";
+	}
+	function worst(){
+		document.getElementById("Modalf6").style.display="none";
+	}
+	
+
